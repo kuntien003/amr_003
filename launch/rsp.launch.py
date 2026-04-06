b783947df32bd454fc3cf5647b8eb1083dd658dc
kuntien003 <tienkun003@gmail.com>
2026-04-06 14:48:19 +0700

    Change project name

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
+    pkg_path = os.path.join(get_package_share_directory('amr_003'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file).toxml()
     
@@ -24,7 +24,7 @@ def generate_launch_description():
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
-        executable='robot_state_publisher',
+        executable='robot_state_publisher', 
         output='screen',
         parameters=[params]
     )
