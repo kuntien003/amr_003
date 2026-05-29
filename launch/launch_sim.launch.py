c6211d4e8790a3a76ebd39125931cff2bcfd7149
kuntien003 <tienkun003@gmail.com>
2026-05-29 18:05:03 +0700

    update code

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,46 +2,63 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
 
+    package_name='amr_003'
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='amr_003' #<--- CHANGE ME
-
+    # SỬA LỖI 4: Đổi use_sim_time thành 'true'
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    # SỬA LỖI 1: Xóa đoạn YAML bị dán nhầm, đưa path về chuẩn
+    world_path = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'obstacles.world'
+    )
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                launch_arguments={
+                    'world': world_path,
+                    # [ĐÃ THÊM MỚI]: Truyền file cấu hình vào Gazebo
+                    'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    # SỬA LỖI 3: executable là 'spawner', không phải 'spawner.py'
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
 
+    # SỬA LỖI 2 & 3: Đổi arguments thành 'joint_broad' và executable thành 'spawner'
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
